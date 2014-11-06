c7dc791dcd2d6eb6b5f0c7593657e244e6f80217
Danilo Shiga <daniloshiga@gmail.com>
2014-11-06 17:42:27 -0200

    This should be a class

--- announcelist/api.py
+++ announcelist/api.py
@@ -9,25 +9,41 @@ import httplib
 BASE_URL = 'https://api.dreamhost.com/'
 
 
-def enable_http_debug():
-    httplib.HTTPConnection.debuglevel = 1
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
+class AnnounceList(object):
 
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
+    def enable_http_debug(self):
+        httplib.HTTPConnection.debuglevel = 1
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
 
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
 
-def test():
-    params = {
-        'key': '6SHU5P2HLDAYECUM',
-        'cmd': 'user-list_users_no_pw',
-        'format': 'json'
-    }
+    @classmethod
+    def test(self):
+        params = {
+            'key': '6SHU5P2HLDAYECUM',
+            'cmd': 'user-list_users_no_pw',
+            'format': 'json'
+        }
 
-    return requests.get(BASE_URL, params=params, verify=False)
+        return requests.get(BASE_URL, params=params, verify=False)
 
+    def list_lists(self):
+        pass
+
+    def list_subscribers(self):
+        pass
+
+    def add_subscriber(self):
+        pass
+
+    def remove_subscriber(self):
+        pass
+
+    def post_announcement(self):
+        pass
 
 if __name__ == "__main__":
-    print test()
+    print AnnounceList.test()
